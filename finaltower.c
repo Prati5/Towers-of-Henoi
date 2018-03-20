985228c24669fe8e18566130f0a70fb9573b5610
Pratima <36811275+Prati5@users.noreply.github.com>
2018-03-20 17:47:44 +0530

    Add files via upload

---
+++ finaltower.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+// n represents the number of disks
+// A represents the starting disk
+// B represents the auxiliary disk
+// E represents the destination(End) disk.
+void TowerOfHenoi(int n,char from,char to,char aux)
+  {
+
+    //Check whether disk is inserted, if yes,recursively call the function
+
+        if(n>=1)
+
+          {
+
+
+            // first recursive call moves n-1 disks from initial peg B to auxiliary A using E.
+            //After that call n-1 disks are in auxiliary A tower and the beginning tower B contains the nth disk i.e largest disk
+            //Now move the disk from "B" to end tower(destination) "E".
+             TowerOfHenoi(n-1,from,aux,to);
+             printf("Move disk %d from %c to %c\n",n,from,to);
+             // Call  TowerOfHenoi n-1 disk from auxiliary A  to E peg using B.
+             TowerOfHenoi(n-1,aux,to,from);
+         }
+
+
+        // after all these the Peg "E" contains all disks in order of size.
+
+   }
+int main()
+  {
+      int n;
+
+      printf("Enter the number of disks\n");
+      scanf("%d",&n);
+      // Call the function of Tower of Henoi
+      TowerOfHenoi(n,'A','E','B');
+      return 0;
+  }
+
+
